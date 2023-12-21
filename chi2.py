72c146fcb51f61147b4ba368fc181d6c08c9cb14
yassinekrika <yassinekrika2016@gmail.com>
2023-12-21 10:38:45 +0100

    chi2

--- chi2.py
+++ chi2.py
@@ -1,40 +1,20 @@
 from PIL import Image
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.stats import chi2_contingency
 
-img = Image.open('/home/yassg4mer/Project/SAFM/bikes.bmp')
+# Load the image and convert it to grayscale
+img = Image.open('/home/yassg4mer/Project/SAFM/plane.bmp')
 img_gray_scale = img.convert('L')
 img_matrix = np.array(img_gray_scale)
 
-rows, cols = img_matrix.shape
+flattened_img = img_matrix.flatten()
 
-group_size = 8
+histogram, bin_edges = np.histogram(flattened_img, bins=range(256), range=(0, 256))
 
-pixel_groups = []
-for i in range(0, rows, group_size):
-    # Iterate over the columns
-    for j in range(0, cols, group_size):
-            # Extract a 4x4 pixel group
-        group = img_matrix[i:i + group_size, j:j + group_size]
-            # Append the group to the list
-        pixel_groups.append(group)
+expected = np.full_like(histogram, len(flattened_img) / 256)
 
-# calulate histogram for group one 
-histograms = []
-for group in pixel_groups:
-    histogram = np.histogram(group.flatten(), bins=range(256), range=(0, 256))
-    histograms.append(histogram)
-
-# show histogram for group one
-# plt.plot(histograms[0][0])    
-# plt.show()
-
-# print(histograms)
-
-# calculate chi2 for each group
-stat, p, do, expected = chi2_contingency(histograms)
-
+stat, p,_, _= chi2_contingency([histogram, expected])
+ 
 alpha = 0.05
 if p <= alpha:
     print('Dependent (reject H0)')
